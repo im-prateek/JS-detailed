8b58447aa519050e21cb0156f7444b1c52a0d129
Prateek Maddheshiya <prateekkumar247247@gmail.com>
2024-08-31 21:12:40 +0530

    let var const, console.table,

--- test.js
+++ test.js
@@ -1,1 +1,13 @@
 // node js installed
+
+let email = "prateek@gmail.com"
+let password = "2134"
+let city = "bengaluru"
+
+console.table([email, password, city]);
+
+/*
+Never use var in your code instead of let because of block and functional scope issues.
+# console.table([group of variables you want to show in tabular form])
+*/
+
