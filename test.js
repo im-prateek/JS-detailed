d709341b06cad7c5fe87547bbc2835c3d3fe050b
Prateek Maddheshiya <prateekkumar247247@gmail.com>
2024-09-01 20:35:29 +0530

    use strict, alert,

--- test.js
+++ test.js
@@ -1,13 +1,21 @@
 // node js installed
 
+/*
+*/
 let email = "prateek@gmail.com"
 let password = "2134"
 let city = "bengaluru"
 
 console.table([email, password, city]);
 
-/*
-Never use var in your code instead of let because of block and functional scope issues.
-# console.table([group of variables you want to show in tabular form])
-*/
+//Never use var in your code instead of let because of block and functional scope issues.
+//console.table([group of variables you want to show in tabular form])
+
+"use strict" //treat all js code as newer version
+
+alert(3+3) //we are using nodejs, not browser
+
+console.log(3);
+
+
 
