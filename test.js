171a0442d9dfd749cf2022b719c6c6638ed48ad1
Prateek Maddheshiya <prateekkumar247247@gmail.com>
2024-09-03 11:15:15 +0530

    datatype conversion

--- test.js
+++ test.js
@@ -16,10 +16,40 @@ alert(3+3) //we are using nodejs, not browser
 
 console.log(3);
 
-*/
-
 console.log(typeof undefined); //underfined
 console.log(typeof null); //object
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber); //MaN means not a number
+
+// if score = null then number conversion of null is 0
+
+//number conversion of the following
+// "33" => 33
+// "33abc" => NaN (not a number)
+// true => 1 fasle => 0
+// null => 0
+
+*/
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//boolean conversion
+// 1 => true; 0=> false
+// "" => false
+// "ram" => true
+
+
 
 
 
