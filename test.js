ca8c305cf072400fa76d958fb843b9b5663dce13
Prateek Maddheshiya <prateekkumar247247@gmail.com>
2024-09-23 21:28:24 +0530

    random min max

--- test.js
+++ test.js
@@ -156,5 +156,7 @@ console.log(Math.min(4,3,6,8));// minimum
 console.log(Math.random());
 console.log((Math.random()*10) + 1);
 
+const min = 10
+const max = 20
 
-
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
